28f81bbcc852985e598de33e826d67471ed6fe7c
M7MDSG76 <mohammed.s.alghanmi@gmail.com>
2021-12-31 17:09:25 +0300

    Add project files.

---
+++ AssignmntQ1.ii/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AssignmntQ1.ii
+{
+    class Program
+    {
+        //Q1.2.2
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************WELLCOME TO THE RANDOM NUMBERS GENERATOR*******************");
+            Console.WriteLine("-Enter the number of Random numbers: ");
+            int RandomNum = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            for (int i = 0; i < RandomNum; i++)
+            {
+                Random X = new Random();
+                int Randomnum = X.Next();
+                Console.WriteLine($"*******************Random Numbers___#{i+1}");
+                Console.WriteLine(Randomnum+"\n\n");
+            }
+        }
+    }
+}
